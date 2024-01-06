519cd6153db49f7831df48a30cbd0da56fd898b4
Niclas <niclastanskanen@hotmail.com>
2024-01-06 20:02:41 +0100

    Add getSimilarProducts function to retrieve similar products

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -73,3 +73,21 @@ export async function getAllProducts() {
     console.error(error);    
   }
 }
+
+export async function getSimilarProducts(productId: string) {
+  try {
+    connectToDB();
+
+    const currentProduct = await Product.findById(productId);
+
+    if(!currentProduct) return null;
+
+    const similarProducts = await Product.find({
+      _id: { $ne: productId },
+    }).limit(3);
+
+    return similarProducts
+  } catch (error) {
+    console.error(error);    
+  }
+}
