5ae9980ca8194521748b4174bf8f78e5aebdc153
Niclas <niclastanskanen@hotmail.com>
2024-01-06 18:35:31 +0100

    Add getProductById function to retrieve product by ID

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -47,3 +47,17 @@ export async function scrapeAndStoreProduct(productUrl: string) {
     throw new Error(`Failed to create/update product: ${error.message}`)
   }
 }
+
+export async function getProductById(productId: string) {
+  try {
+    connectToDB();
+
+    const product = await Product.findOne({ _id: productId });
+
+    if (!product) return null;
+
+    return product;
+  } catch (error) {
+    console.error(error);
+  }
+}
