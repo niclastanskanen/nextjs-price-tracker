12729375c19f3f30ebfca6c31cffc8fc8e4b2ffe
Niclas <niclastanskanen@hotmail.com>
2024-01-06 19:18:25 +0100

    Add getAllProducts function to retrieve all products

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -61,3 +61,15 @@ export async function getProductById(productId: string) {
     console.error(error);
   }
 }
+
+export async function getAllProducts() {
+  try {
+    connectToDB();
+
+    const products = await Product.find();
+
+    return products
+  } catch (error) {
+    console.error(error);    
+  }
+}
