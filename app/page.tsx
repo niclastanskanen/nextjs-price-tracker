5e02187f1597082465a8edbe518b135e655b3a6d
Niclas <niclastanskanen@hotmail.com>
2024-01-06 19:18:14 +0100

    Add dynamic rendering of product cards

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import Image from "next/image"
 
 import Searchbar from "@/components/Searchbar"
 import HeroCarousel from "@/components/HeroCarousel"
+import { getAllProducts } from "@/lib/actions"
+import ProductCard from "@/components/ProductCard"
 
-const Home = () => {
+const Home = async () => {
+  const allProducts = await getAllProducts()
+
   return (
     <>
       <section className="px-6 md:px-20 py-24">
@@ -36,9 +40,8 @@ const Home = () => {
           Trending
         </h2>
         <div className="flex flex-wrap gap-x-8 gap-y-16">
-          {['Apple Iphone 15', 'Book', 'Sneakers'].map
-          ((product) => (
-            <div>{product}</div>
+          {allProducts?.map((product) => (
+            <ProductCard key={product._id} product={product} />
           ))}
         </div>
       </section>
