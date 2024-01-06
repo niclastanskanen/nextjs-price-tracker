680e9d974e1a1625c14295b27617af830d16ad27
Niclas <niclastanskanen@hotmail.com>
2024-01-06 15:39:05 +0100

    Add product scraping functionality

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import * as cheerio from 'cheerio';
 
+import { extractCurrency, extractPrice } from '../utils';
+
 export async function scrapeAmazonProduct(url: string) {
   if(!url) return;
 
@@ -23,8 +25,38 @@ export async function scrapeAmazonProduct(url: string) {
   try {
     // Fetch the product page
     const response = await axios.get(url, options);
+    const $ = cheerio.load(response.data);
 
-    console.log('response', response.data);
+    // Extract the product title
+    const title = $('#productTitle').text().trim();
+    const currentPrice = extractPrice(
+      $('.priceToPay span.a-price-whole'),
+      $('.a.size.base.a-color-price'),
+      $('.a-button-selected .a-color-base'),
+    )
+
+    const originalPrice = extractPrice(
+      $('#priceblock_ourprice'),
+      $('.a-price.a-text-price span.a-offscreen'),
+      $('#listPrice'),
+      $('#priceblock_dealprice'),
+      $('.a-size-base.a-color-price')
+    )
+
+    const outOfStock = $('#availability span').text().trim().toLowerCase() === 'currently unavailable';
+
+    const images = 
+      $('#imgBlkFront').attr('data-a-dynamic-image') ||
+      $('#landingImage').attr('data-a-dynamic-image') ||
+      '{}'
+
+    const imagesUrls = Object.keys(JSON.parse(images))
+
+    const currency = extractCurrency($('.a-price-symbol'))
+    
+    const discountRate = $('.savingsPercentage').text().replace(/&nbsp;|\s|[-%]/g, '');
+
+    console.log({ title, currentPrice, originalPrice, outOfStock, imagesUrls, currency, discountRate });
   } catch (error: any) {
     throw new Error(`Failed to scrape product: ${error.message}`)
   }
